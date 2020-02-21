7e537da65d7b62ae0c688884dc3b6909c1c2eda3
FrancescoDAlterio <francesco.dalterio1992@gmail.com>
2020-02-21 15:46:47 +0100

    implemented __initializeDBstructure with the current required tables (draft, tables could change)

--- imports/storage.py
+++ imports/storage.py
@@ -1,4 +1,5 @@
 from .utilities.singleton import Singleton
+from .utilities.databaseManager import DatabaseManager
 from pathlib import Path
 import re
 import sys
@@ -9,8 +10,9 @@ import datetime
 class Storage(metaclass=Singleton):
     def __init__(self):
 
-        self.database_path = self.__createDB()[1]
-        print('aaa')
+        self.__database_path = self.__createDB()[1]
+        self.__DBM = DatabaseManager(self.__database_path)
+        self.__initializeDBstructure()
 
 
 
@@ -57,3 +59,105 @@ class Storage(metaclass=Singleton):
 
         print("created DB: ",db_path)
         return 0,db_path
+
+
+
+    def __initializeDBstructure(self):
+        print("Initializing DB...")
+        sql_create_table_devices = """ CREATE TABLE IF NOT EXISTS devices (
+                                            BLE_address text PRIMARY KEY,
+                                            name text ,
+                                            MAC_address text UNIQUE 
+                                        ); """
+
+        sql_create_table_events = """CREATE TABLE IF NOT EXISTS events (
+                                        event_id integer PRIMARY KEY AUTOINCREMENT,
+                                        submitter_id text NOT NULL,
+                                        timestamp integer NOT NULL,
+                                        type integer NOT NULL,
+                                        FOREIGN KEY (submitter_id) REFERENCES devices (BLE_address)
+                                    );"""
+
+        sql_create_table_TE_message_sent = """CREATE TABLE IF NOT EXISTS typeEvent_message_sent (
+                                        id integer PRIMARY KEY AUTOINCREMENT,
+                                        event_id text NOT NULL,
+                                        sender_id text NOT NULL,
+                                        receiver_id text NOT NULL,
+                                        next_hop_id text,
+                                        message_type text NOT NULL,
+                                        payload text NOT NULL,
+                                        FOREIGN KEY (event_id) REFERENCES events (event_id)
+                                    );"""
+
+        sql_create_table_TE_message_received = """CREATE TABLE IF NOT EXISTS typeEvent_message_received (
+                                        id integer PRIMARY KEY AUTOINCREMENT,
+                                        event_id text NOT NULL,
+                                        sender_id text NOT NULL,
+                                        receiver_id text NOT NULL,
+                                        prev_hop_id text,
+                                        message_type text NOT NULL,
+                                        payload text NOT NULL,
+                                        FOREIGN KEY (event_id) REFERENCES events (event_id)
+                                    );"""
+
+        sql_create_table_TE_incoming_connection_attempts = """CREATE TABLE IF NOT EXISTS typeEvent_incoming_connection_attempts (
+                                                id integer PRIMARY KEY AUTOINCREMENT,
+                                                event_id text NOT NULL,
+                                                requester_id text NOT NULL,
+                                                notes text,
+                                                FOREIGN KEY (event_id) REFERENCES events (event_id)
+                                            );"""
+
+        sql_create_table_TE_outgoing_connection_attempts = """CREATE TABLE IF NOT EXISTS typeEvent_outgoing_connection_attempts (
+                                                id integer PRIMARY KEY AUTOINCREMENT,
+                                                event_id text NOT NULL,
+                                                target_id text NOT NULL,
+                                                notes text,
+                                                FOREIGN KEY (event_id) REFERENCES events (event_id)
+                                            );"""
+
+        sql_create_table_TE_connection_attempts_outcomes = """CREATE TABLE IF NOT EXISTS typeEvent_connection_attempts_outcomes (
+                                                id integer PRIMARY KEY AUTOINCREMENT,
+                                                event_id text NOT NULL,
+                                                requester_id text NOT NULL,
+                                                target_id text NOT NULL,
+                                                outcome text NOT NULL,
+                                                notes text,
+                                                FOREIGN KEY (event_id) REFERENCES events (event_id)
+                                            );"""
+
+        sql_create_table_TE_device_up = """CREATE TABLE IF NOT EXISTS typeEvent_device_up (
+                                                id integer PRIMARY KEY AUTOINCREMENT,
+                                                event_id text NOT NULL,
+                                                FOREIGN KEY (event_id) REFERENCES events (event_id)
+                                            );"""
+
+        sql_create_table_TE_assume_role = """CREATE TABLE IF NOT EXISTS typeEvent_assume_role (
+                                                        id integer PRIMARY KEY AUTOINCREMENT,
+                                                        event_id text NOT NULL,
+                                                        role text NOT NULL,
+                                                        FOREIGN KEY (event_id) REFERENCES events (event_id)
+                                                    );"""
+
+        sql_create_table_TE_scan = """CREATE TABLE IF NOT EXISTS typeEvent_scan (
+                                                        id integer PRIMARY KEY AUTOINCREMENT,
+                                                        event_id text NOT NULL,
+                                                        start_or_stop text NOT NULL,
+                                                        FOREIGN KEY (event_id) REFERENCES events (event_id)
+                                                    );"""
+
+
+        self.__DBM.create_table(sql_create_table_devices)
+        self.__DBM.create_table(sql_create_table_events)
+        self.__DBM.create_table(sql_create_table_TE_message_sent)
+        self.__DBM.create_table(sql_create_table_TE_message_received)
+        self.__DBM.create_table(sql_create_table_TE_incoming_connection_attempts)
+        self.__DBM.create_table(sql_create_table_TE_outgoing_connection_attempts)
+        self.__DBM.create_table(sql_create_table_TE_connection_attempts_outcomes)
+        self.__DBM.create_table(sql_create_table_TE_device_up)
+        self.__DBM.create_table(sql_create_table_TE_assume_role)
+        self.__DBM.create_table(sql_create_table_TE_scan)
+
+
+
+
--- imports/utilities/databaseManager.py
+++ imports/utilities/databaseManager.py
@@ -2,4 +2,28 @@ import sqlite3
 
 
 class DatabaseManager():
-    pass
+
+    def __init__(self,sql_database_path):
+        self.__sql_database_path = sql_database_path
+        self.__conn = sqlite3.connect(self.__sql_database_path)
+
+
+
+
+
+    def create_table(self, create_table_sql):
+        """ create a table from the create_table_sql statement
+        :param conn: Connection object
+        :param create_table_sql: a CREATE TABLE statement
+        :return:
+        """
+        try:
+            cur = self.__conn.cursor()
+            cur.execute(create_table_sql)
+        except Exception as e:
+            print('E: error occurred while executing the following sql command: ',create_table_sql)
+            raise e
+        pass
+
+
+
